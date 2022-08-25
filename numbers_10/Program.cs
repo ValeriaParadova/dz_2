61570ab8a2809cb2dfbbeca494b2c5f1f8acea37
Valeria <lerka.paradova@mail.ru>
2022-08-26 00:26:41 +0300

    add changes folder

--- numbers_10/Program.cs
+++ numbers_10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// Например:
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int number1 = number / 10;
+int number2 = number1 % 10;
+
+Console.WriteLine(number2);
+ 
