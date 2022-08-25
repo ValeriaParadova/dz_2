d25b92de9e3f60b1ef536ef3acdd86da3d09ee98
Valeria <lerka.paradova@mail.ru>
2022-08-26 00:38:09 +0300

    add numbers_13

---
+++ numbers_13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+// Например:
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int number = new Random().Next(1, 100000);
+
+Console.WriteLine(number);
+
+
+if (number > 99)
+{
+    string number1 = ("");
+
+    number1 = Convert.ToString(number);
+    Console.WriteLine(number1[2]);
+}
+
+else
+
+{
+    Console.WriteLine("no third digit");
+}
